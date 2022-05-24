788ea11f672e38436fb7f26be962e56419d81744
Jackie <jye0325@gmail.com>
2022-05-24 12:45:44 -0400

    commit tree e0b551ee2c8f1004db2df926c8f882632523b19c

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, "");
+  };
+  static titleize(string) {
+    const exceptionSet = new Set(["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]);
+    const wordArray = string.split(" ").map((word) => 
+      exceptionSet.has(word)?word:(word.charAt(0).toUpperCase() + word.slice(1))
+    );
+    wordArray[0] = wordArray[0].charAt(0).toUpperCase() + wordArray[0].slice(1);
+
+    return wordArray.reduce((a,b) => a + " " + b);
+  };
 }
